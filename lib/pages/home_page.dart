0094503b84c7f247c4be59a9481287da8d3a49a4
JoseCima <josegcima@gmail.com>
2022-06-02 16:18:41 -0500

    Video 6 y 7

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,11 +21,19 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
        backgroundColor: Color(0xFFF9F9F9),
       appBar: _appBar(),
-      body: Column(
-        children: <Widget>[
-          _labels(),
-          _historias(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _labels(),
+            _historias(),
+            Divider(
+              color: Colors.grey,
+              height: 0,
+              thickness: 0.5,
+            ),
+            _posts(),
+          ],
+        ),
       ),
     );
   }
@@ -45,7 +53,7 @@ class _HomePageState extends State<HomePage> {
     backgroundColor: Color(0xFFF9F9F9),
     title: Container(
       margin: EdgeInsets.only(top: 10),
-      child: SizedBox(child: Image.asset('assets/img/logo_instagram.jpg'),
+      child: SizedBox(child: Image.asset('assets/img/logo-instagram.png'),
       height: 40,),
     ),
     leading: IconButton(
@@ -126,6 +134,131 @@ Widget _historias(){
      ],
    );
 }
+
+Widget _posts(){
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    
+    height: 400,
+
+    child: ListView.builder(
+      itemCount: 50,
+      itemBuilder: (context, i){
+        return _crearPost();
+      }
+    ),
+  );
+}
+
+Widget _crearPost(){
+
+  return Container(
+    child: Column(
+      children: <Widget>[
+        Container(
+          child: Row(
+            children: <Widget>[
+              Container(
+         
+         child: Container(
+           padding: EdgeInsets.only(
+             top: 12,
+             left: 18,
+             bottom: 12,
+             right: 12
+           ),
+           child: ClipRRect(
+             borderRadius: BorderRadius.circular(50),
+             child: Image(
+               image: NetworkImage('https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1121.jpg') ,
+               height: 45,
+               width: 45,
+               fit: BoxFit.cover,
+               
+             ),
+           ),
+         ),
+       ),
+              Text('Memo', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+              Expanded(child: SizedBox()),
+              IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz), iconSize: 30,),
+              
+            
+            ],
+          ),
+        ),
+        FadeInImage(placeholder: AssetImage('assets/img/loading.gif'), image: NetworkImage('http://placeimg.com/640/480/nightlife')),
+        Container(
+          padding: EdgeInsets.only(top: 5, left: 7, right: 7, bottom: 1),
+          child: 
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: <Widget>[
+                    IconButton(onPressed: (){}, icon: Image(image: AssetImage('assets/img/heart.png'), width: 30)),
+                    IconButton(onPressed: (){}, icon: Image(image: AssetImage('assets/img/comment.png'), width: 30)),
+                    IconButton(onPressed: (){}, icon: Image(image: AssetImage('assets/img/send.png'), width: 30))
+                ],
+              ),
+              IconButton(onPressed: (){}, icon: Image(image: AssetImage('assets/img/save_o.png'), width: 30)),
+            ],
+          )
+          
+        )
+        ,Container(
+          padding: EdgeInsets.only(left: 17, right: 17, bottom: 10),
+          color: Colors.white,
+          child: Row(
+            children: <Widget>[
+              Text('Likes By Memo, Alex 118, others')
+            ],
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          padding: EdgeInsets.only(left: 17, right: 17, bottom: 10),
+          child: Column(
+            children: <Widget>[
+              RichText(
+                
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Memo',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15
+                      )
+                    ),
+                    TextSpan(
+                      text: ' Occaecati volutatem accusantium et quos odit doloribus. Consequatur impedit culpa sit repellendus iure qui. Nobis sit aliquid qui ad. Id sapiente velit reprehenderit molestiae rerum iusto. Nihil est eum.',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15
+                      )
+                    )
+                  ]
+                )
+                )
+              , Container(
+                alignment: Alignment.topLeft,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Text('Junio 02', style: TextStyle(color: Colors.grey, fontSize: 18)),
+
+                
+              )
+            ],
+          ),
+        )
+      
+      ],
+    ),
+  );
+}
+
 }
 
 
